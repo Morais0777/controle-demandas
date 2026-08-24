882584bb678327b0f6bac138ea88d2cedb298072
Maycon Moaris <maycon.morais0777@gmail.com>
2026-08-24 16:57:07 -0300

    fix: corrige anonKey para JWT legacy

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 
 const SUPABASE_CONFIG = {
   url: 'https://eanmiqacaphchqhrbrvt.supabase.co',  // ← Cole sua Project URL
-  anonKey: 'sb_publishable_KK6ywEpqwkMHa6MuCFWomA_GUsbHEzB'                // ← Cole sua anon/public key
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhbm1pcWFjYXBoY2hxaHJicnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzOTQ1NjAsImV4cCI6MjEwMTk3MDU2MH0.kDXPqkit_hOV8NfRkwBPlUW7KFQXOZij4pU37PchGWo'                // ← Cole sua anon/public key
 };
 
 // NÃO compartilhe este arquivo com credenciais preenchidas
